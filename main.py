eeceff73c4bb639b916fb88e7a2d7e57cec0d26c
alexnakov <59810189+alexnakov@users.noreply.github.com>
2020-11-03 22:58:26 +0000

    Letterbox can be dragged with the mouse

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ class LetterBox:
         self.x = x
         self.y = y
         self.image = image
+        self.rect = Rect((self.x, self.y), (100, 100))
+        self.pressed = False
 
     def move_to(self, surface, target_pos, speed):
+        """ Moves a letterbox to specific coords with a specified speed """
         displacement = int(sqrt((target_pos[1] - self.y)**2 + (target_pos[0] - self.x)**2))
         direction = atan2(target_pos[1] - self.y, target_pos[0] - self.x)
-
         dx = cos(direction) * speed
         dy = sin(direction) * speed
 
@@ -33,30 +35,38 @@ class LetterBox:
             pygame.display.update()
             clock.tick(FPS)
 
+    def drag(self, surface):
+        """ When called, it allows for the user to drag the letter with their mouse """
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        mouse_dx, mouse_dy = pygame.mouse.get_rel()
+
+        if pygame.mouse.get_pressed() == LEFT_PRESS and self.rect.collidepoint(mouse_x, mouse_y) and not self.pressed:
+            self.pressed = True
+        elif pygame.mouse.get_pressed() == LEFT_PRESS and self.pressed:
+            surface.fill(BLACK, rect=self.rect)
+            self.rect.x += mouse_dx
+            self.rect.y += mouse_dy
+            self.x += mouse_dx
+            self.y += mouse_dy
+            surface.blit(self.image, (self.x, self.y))
+        elif pygame.mouse.get_pressed() != LEFT_PRESS:
+            self.pressed = False
+
 
 def main():
-    pygame.init()
-
-    root = pygame.display.set_mode(WINDOW_SIZE)
-
-    img = pygame.image.load('letters/A.png')
-    letter_a = LetterBox(20, 20, img)
-    root.blit(img, (letter_a.x, letter_a.y))
 
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-        if pygame.key.get_pressed()[K_a] == 1:
-            letter_a.move_to(root, (200, 150), 8)
-        elif pygame.key.get_pressed()[K_b] == 1:
-            letter_a.move_to(root, (20, 20), 8)
 
         pygame.display.update()
         clock.tick(FPS)
 
 
 if __name__ == '__main__':
+    pygame.init()
+    root = pygame.display.set_mode(WINDOW_SIZE)
     clock = pygame.time.Clock()
     main()
